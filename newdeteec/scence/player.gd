db1b348f38a3788a645d26be45fede00a9b0f664
ADMIN <datnguyenbi98@gmail.com>
2025-06-08 09:58:11 +0700

    player

---
+++ newdeteec/scence/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+
+
+func _physics_process(delta: float) -> void:
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
